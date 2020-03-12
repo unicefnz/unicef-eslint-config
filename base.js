f6b49667717e61f39fadb5ab49f6b7405d51bdc3
Asher Foster <me@asherfoster.com>
2020-03-13 11:34:07 +1300

    Disable more rules

--- base.js
+++ base.js
@@ -4,6 +4,9 @@
 module.exports = {
   rules: {
     'comma-dangle': 0,
+    'react/jsx-filename-extension': 0,
+    'prefer-template': 0,
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
     'max-len': ['error', 140, 2, {
       ignoreUrls: true,
       ignoreComments: false,
