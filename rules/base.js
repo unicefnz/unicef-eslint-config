e24ba8171407fa66b4d6818802a1b98c655f36fc
Asher Foster <asher@unicef.org.nz>
2020-08-04 15:03:03 +1200

    Fix spaced-comment rule

--- rules/base.js
+++ rules/base.js
@@ -27,5 +27,19 @@ module.exports = {
   'no-plusplus': 0,
 
   // This should be the author's decision
-  'prefer-template': 0
+  'prefer-template': 0,
+
+  // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L501
+  'spaced-comment': ['error', 'always', {
+    line: {
+      exceptions: ['-', '+'],
+      markers: ['=', '!', '/'], // space here to support sprockets directives
+    },
+    block: {
+      exceptions: ['-', '+'],
+      // Support @__PURE__ comments, used by webpack tooling
+      markers: ['=', '!', ':', '::', '@__PURE__'], // space here to support sprockets directives and flow comment types
+      balanced: true,
+    }
+  }]
 };
--- rules/typescript.js
+++ rules/typescript.js
@@ -1,18 +1,4 @@
 module.exports = {
   // This should be handled by the compiler, not linter
-  'consistent-return': 0,
-
-  // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L501
-  'spaced-comment': ['error', 'always', {
-    line: {
-      exceptions: ['-', '+'],
-      markers: ['=', '!', '/'], // space here to support sprockets directives
-    },
-    block: {
-      exceptions: ['-', '+'],
-      // Add @__ to support @__PURE__ comments, used by webpack tooling
-      markers: ['=', '!', ':', '::', '@__'], // space here to support sprockets directives and flow comment types
-      balanced: true,
-    }
-  }],
+  'consistent-return': 0
 };
