9e13015cdcbdf94541799c9c0f318a04f3efc545
Asher Foster <asher@unicef.org.nz>
2021-02-02 12:10:31 +1300

    fix(comma-dangle): Override airbnb-typescript's updated rule

--- src/rules/typescript.js
+++ src/rules/typescript.js
@@ -1,5 +1,8 @@
 module.exports = {
   // These should be handled by the compiler, not linter
   'consistent-return': 0,
-  'import/no-unresolved': 0
+  'import/no-unresolved': 0,
+
+  // Style preference
+  '@typescript-eslint/comma-dangle': ['error', 'never']
 };
